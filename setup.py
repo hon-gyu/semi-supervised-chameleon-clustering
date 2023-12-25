479f4b0621876382e51c26e2ce1b467ef3b6a6de
HyLin <linerwang1997@gmail.com>
2023-12-25 16:26:26 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='semi_supervised_chameleon_clustering',
+    version='0.1',
+    packages=find_packages(),
+    license='Your License',
+    long_description=open('README.md').read(),
+    install_requires=[
+        
+    ],
+)
